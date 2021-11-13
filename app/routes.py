5acaea09a318a83e0773bde02766d6de742928fb
Casey Lewis <caseylewis@Caseys-Air.fios-router.home>
2021-11-13 00:12:45 -0500

    program works for set n

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,26 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
+import main
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html", title='Home Page')
+    n = 3
+    return render_template("index.html", title='Home', n=n)
+
+# Handle the matrix
+@app.route('/matrix-handler', methods=['GET', 'POST'])
+def handle_matrix():
+    if request:
+        flash("request exists")
+    n = 3
+    matrix = [[0 for j in range(n)] for k in range(n)]
+    for x in range(n):
+        for y in range(n):
+            row = str(x)
+            col = str(y)
+            id = row + "_" + col
+            matrix[x][y] = int(request.form.get(id))
+    determinant = main.det(matrix, n)
+    flash("The determinant is " + str(determinant))
+    return redirect(url_for('index'))
