b6a2da8a10a59d6fc33cd0d619f54c2a33b13a14
Casey Lewis <caseylewis@Caseys-MacBook-Air.local>
2021-12-18 16:04:49 -0500

    Check orthogonality

--- app/routes.py
+++ app/routes.py
@@ -83,10 +83,64 @@ def normalize_set():
             valid = False
     if (valid):
         normal = main.normalize_set(matrix)
-        result = "The determinant is " + str(normal)
+        result = "The normalized set is " + str(normal)
     elif (zero_vector):
         result = "The zero vector can't be normalized"
     else:
         result = "Input was invalid. Please try again."
         valid = False
     return render_template("normalize.html", title='Normalize', size=size, vectors=vectors, matrix=matrix, result=result, valid=valid, normal=normal)
+
+
+    # default orthogonals page
+@app.route('/orthogonals', methods=['GET', 'POST'])
+def orthogonals():
+    size = 2
+    vectors = 2
+    return render_template("orthogonals.html", title='Orthogonals', size=size, vectors=vectors)
+
+
+# resize normalize page
+@app.route('/orthogonals/<int:size>/<int:vectors>', methods=['GET', 'POST'])
+def ortho_size(size, vectors):
+    return render_template("orthogonals.html", title='Orthogonals', size=size, vectors=vectors)
+
+
+# Handle the normalize function
+@app.route('/check-ortho', methods=['GET', 'POST'])
+def check_orthogonal():
+    vectors = int(request.form.get('vectors'))
+    size = int(request.form.get('size'))
+    valid = True
+    zero_vector = False
+    result = ""
+    matrix = [[0 for j in range(size)] for k in range(vectors)]
+    normal = []
+    for x in range(vectors):
+        not_zero_check = False
+        for y in range(size):
+            row = str(y)
+            col = str(x)
+            id = row + "_" + col
+            try:
+                int(request.form.get(id))
+                matrix[x][y] = int(request.form.get(id))
+                if matrix[x][y] != 0:
+                    not_zero_check = True
+            except ValueError:
+                valid = False
+                matrix[x][y] = 0
+        if not not_zero_check:
+            zero_vector = True
+            valid = False
+    if (valid):
+        if (main.is_orthogonal_set(matrix)):
+            result = "The set is orthogonal!"
+        else:
+            result = "The set is not orthogonal!"
+    elif (zero_vector):
+        result = "The zero vector can't be normalized."
+    else:
+        result = "Input was invalid. Please try again."
+        valid = False
+    return render_template("orthogonals.html", title='Orthogonals', size=size, vectors=vectors, matrix=matrix, result=result, valid=valid, normal=normal)
